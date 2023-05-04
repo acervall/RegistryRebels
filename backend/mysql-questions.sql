8580f2ac8f76b56584c9eaf9ca2f7a842fba9f99
acervall <ameliecervall@gmail.com>
2023-05-04 16:33:01 +0200

    inner join

--- backend/mysql-questions.sql
+++ backend/mysql-questions.sql
@@ -15,6 +15,6 @@ SELECT * FROM selectedproduct;
 SELECT * FROM user;
 
 SELECT *
-FROM selectedproducts
-    INNER JOIN product ON product.product_Id = selectedProducts.selectedProductsP_Id
-    INNER JOIN list ON list.list_Id = selectedProducts.selectedProductsList_Id;
+FROM selectedproduct
+    INNER JOIN product ON product.product_Id = selectedProduct.selectedProductP_Id
+    INNER JOIN list ON list.list_Id = selectedProduct.selectedProductList_Id;
