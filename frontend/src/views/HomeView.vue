e21adf1074dd1bea5cd4aace305cc4dfda125cb8
Hanne-Kristin-Steensath <hanne.kristin.steensath@gmail.com>
2023-05-05 16:12:02 +0200

    add category.js, add CategorySelection.vue, HamburgerMenu.vue, Navbar.vue component

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,16 +1,26 @@
 <script>
+  import CategorySelection from '../components/CategorySelection.vue'
   import ProductSection from '../components/ProductSection.vue'
   import UserListSection from '../components/UserListSection.vue'
+  import NavBar from '../components/NavBar.vue'
 
   export default {
     components: {
       ProductSection,
-      UserListSection
+      UserListSection,
+      CategorySelection,
+      NavBar,
     },
   }
 </script>
 
 <template>
-  <ProductSection />
-  <UserListSection/>
+  <div>
+    <NavBar />
+    <CategorySelection />
+    <ProductSection />
+    <UserListSection />
+
+    <ProductSection />
+  </div>
 </template>
