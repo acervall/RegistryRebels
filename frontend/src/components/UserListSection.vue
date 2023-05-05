dd8ccb02a27c06c072206dfc5378ab257417a29a
Thiti <thiti.m90@gmail.com>
2023-05-05 16:59:10 +0200

    Fixed that you can see and add lists from db

--- frontend/src/components/UserListSection.vue
+++ frontend/src/components/UserListSection.vue
@@ -6,18 +6,60 @@
     data() {
       return {
         userList: [],
+        listName: "",
+        listU_Id: "" ,
       }
     },
     methods: {
-      async getProducts() {
-        const data = await fetch('http://localhost:3000//api/user-product-list')
-        this.productList = await data.json()
+        // Hämtar alla listor från databasen
+      async getUserList() {
+        const data = await fetch('http://localhost:3000/api/user-product-list')
+        this.userList = await data.json()
         console.log(this.userList)
       },
+      async submitForm(){
+        // Skickar lista till databasen
+        console.log("Innan send", this.listName, this.listU_Id)
+    const send = await fetch('http://localhost:3000/api/user-product-list',{
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+        
+            listName: this.listName,
+            listU_Id: this.listU_Id
+        })    
+    })
+  
+    const data = await send.json()
+        console.log("Svar från backend: ", data) 
+      }
     },
   }
 </script>
 
 <template>
-  <h1>HEJHEJ</h1>
+<div id="main-container">
+  <h1>Create Wislist</h1>
+    <div>
+        <!-- Formulär -->
+    <form v-on:submit="submitForm">
+        <label for="listName">Namn på listan :</label>
+        <input type="text" id="listName" v-model="listName">
+        <label for="listU_Id">Skriv in id: </label>
+        <input type="number" id="listU_Id" name="listU_Id" v-model="listU_Id">
+        <input type="submit" value="Save list">
+    </form>
+    </div>
+
+    <!-- Lista på alla listor -->
+    <h3>All wishlist</h3>
+    <div id="userList-container"
+    v-for="user in userList"
+    :key="user.list_Id">
+    <p>{{ user.listName }} {{ user.listU_Id }}</p>
+    
+    </div>
+</div>
 </template>
