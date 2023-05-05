5776b265bd5f024c0e09dcad76d268e189570e2c
Johan Leijon <leijon.96@hotmail.com>
2023-05-05 16:42:11 +0200

    Adding ProductSectionUser & fixing some SHIT

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,16 +1,20 @@
 <script>
   import ProductSection from '../components/ProductSection.vue'
   import UserListSection from '../components/UserListSection.vue'
+  import ProductSectionUser from '../components/ProductSectionUser.vue'
 
   export default {
     components: {
       ProductSection,
-      UserListSection
+      UserListSection,
+      ProductSectionUser
     },
   }
 </script>
 
 <template>
   <ProductSection />
-  <UserListSection/>
+  <ProductSectionUser />
+  <UserListSection />
+
 </template>
