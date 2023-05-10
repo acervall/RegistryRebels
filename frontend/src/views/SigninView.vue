8bbc110561b28434ea44552138c9796ea2f0ed89
Thiti <thiti.m90@gmail.com>
2023-05-10 12:40:04 +0200

    Started with form in SigninView

--- frontend/src/views/SigninView.vue
+++ frontend/src/views/SigninView.vue
@@ -5,7 +5,40 @@
 </script>
 
 <template>
-  <h1>SigninView</h1>
+  <h1>Registry Rebels</h1>
+  <form action="" class="loginform">
+    <label for="email">Email</label>
+    <input type="text" />
+    <label for="password">Password</label>
+    <input type="password" />
+    <button type="submit"><p id="singin-btn">Sign in</p></button>
+    <p id="para-and-link">
+      or
+      <a href="länk fråga amelie"> sign up</a>
+    </p>
+  </form>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+
+  input {
+    height: 100%;
+  }
+
+  button {
+    background-color: black;
+  }
+
+  #singin-btn {
+    color: white;
+  }
+
+  #para-and-link {
+    display: flex;
+    justify-content: center;
+  }
+</style>
