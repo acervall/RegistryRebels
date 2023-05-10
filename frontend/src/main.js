abb7d463a4238dee4dd752a7cdcc8ee3790df76c
Marcus Oskarsson <MarcusOskarsson@hotmail.com>
2023-05-10 16:33:41 +0200

    Revert "add icons"
    This reverts commit fb25a6fd6fcb7d65a59d885e8d8390915db2a83e.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,7 @@
 import { createApp } from 'vue'
-import { OhVueIcon, addIcons } from 'oh-vue-icons'
-import { IoCloseCircleOutline, BiStarFill, CoStar } from 'oh-vue-icons/icons'
 
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import shortText from './directives/shortText'
 
-addIcons(IoCloseCircleOutline, BiStarFill, CoStar)
-
-createApp(App)
-  .use(router)
-  .use(store)
-  .component('v-icon', OhVueIcon)
-  .directive('shortText', shortText)
-  .mount('#app')
+createApp(App).use(router).use(store).mount('#app')
