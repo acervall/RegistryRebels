93fb2a0bb75b314f62349fb6ce1f46dbae01595d
Marcus Oskarsson <MarcusOskarsson@hotmail.com>
2023-05-10 16:42:39 +0200

    add icons

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,16 @@
 import { createApp } from 'vue'
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import { BiStarFill, CoStar, IoCloseCircleOutline } from 'oh-vue-icons/icons'
+addIcons(BiStarFill, CoStar, IoCloseCircleOutline)
 
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import shortText from './directives/shortText'
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App)
+  .use(router)
+  .use(store)
+  .directive('shortText', shortText)
+  .component('v-icon', OhVueIcon)
+  .mount('#app')
