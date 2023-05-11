7c7e352a9feaec54d01926623d4f4b53a963d74a
cwacode <williamandersson27@gmail.com>
2023-05-11 08:45:16 +0200

    changes in guestSelectedProduct

---
+++ backend/routes/guestSelectedProduct.js
@@ -0,0 +1,102 @@
+const express = require('express')
+const connection = require('../connection')
+const router = express.Router()
+
+
+// Get all guestselectedproduct
+router.get('/api/guest-selected-products', async (req, res) => {
+  const sql = 'SELECT * FROM guestSelectedProduct';
+  try {
+    await connection.query(sql, (error, results) => {
+      if (error) {
+        console.error(error.sqlMessage);
+      } else {
+        res.json(results);
+      }
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error,
+    });
+  }
+});
+
+// Get a single product by id
+router.get('/api/guest-selected-products/:id', async (req, res) => {
+  const { id } = req.params;
+  const sql = 'SELECT * FROM guestSelectedProduct WHERE guestSelectedProduct_Id = ?';
+  try {
+    await connection.query(sql, [id], (error, results) => {
+      if (error) {
+        console.error(error.sqlMessage);
+        res.status(500).json({ error });
+      } else {
+        if (results.length === 0) {
+          res.status(404).json({ message: 'Product not found' });
+        } else {
+          res.json(results[0]);
+        }
+      }
+    });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+});
+
+
+// CREATE - Add a new guestselectedproduct
+router.post('/api/guest-selected-products', async (req, res) => {
+  const { guestSelectedProductS_Id, guestSelectedProductG_Id, productPurchased } = req.body;
+  const sql = 'INSERT INTO guestSelectedProduct (guestSelectedProductS_Id, guestSelectedProductG_Id, productPurchased) VALUES (?, ?, ?)';
+  try {
+    await connection.query(sql, [guestSelectedProductS_Id, guestSelectedProductG_Id, productPurchased], (error, results) => {
+      if (error) {
+        console.error(error.sqlMessage);
+        res.status(500).json({ error });
+      } else {
+        res.status(201).json({ message: 'Guest selected product added successfully', insertId: results.insertId });
+      }
+    });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+});
+
+// UPDATE - Update a guestselectedproduct by id
+router.put('/api/guest-selected-products/:id', async (req, res) => {
+  const { id } = req.params;
+  const { guestSelectedProductS_Id, guestSelectedProductG_Id, productPurchased } = req.body;
+  const sql = 'UPDATE guestSelectedProduct SET guestSelectedProductS_Id = ?, guestSelectedProductG_Id = ?, productPurchased = ? WHERE guestSelectedProduct_Id = ?';
+  try {
+    await connection.query(sql, [guestSelectedProductS_Id, guestSelectedProductG_Id, productPurchased, id], (error, results) => {
+      if (error) {
+        console.error(error.sqlMessage);
+        res.status(500).json({ error });
+      } else {
+        res.json({ message: 'Guest selected product updated successfully', affectedRows: results.affectedRows });
+      }
+    });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+});
+
+// DELETE - Delete a guestselectedproduct by id
+router.delete('/api/guest-selected-products/:id', async (req, res) => {
+  const { id } = req.params;
+  const sql = 'DELETE FROM guestSelectedProduct WHERE guestSelectedProduct_Id = ?';
+  try {
+    await connection.query(sql, [id], (error, results) => {
+      if (error) {
+        console.error(error.sqlMessage);
+        res.status(500).json({ error });
+      } else {
+        res.json({ message: 'Guest selected product deleted successfully', affectedRows: results.affectedRows });
+      }
+    });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+});
+
+module.exports = router;
---
+++ backend/user.js
@@ -0,0 +1,123 @@
+const express = require('express');
+const connection = require('../connection');
+const router = express.Router();
+const cors= require('cors');
+
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
+
+router.use(cors());
+const port= 3000;
+
+// GET all users
+router.get('/api/user', async (req, res) => {
+  const sql = 'SELECT * FROM user'
+  try {
+    await connection.query(sql, (error, results) => {
+      if (error) {
+        console.error(error.sqlMessage)
+      } else {
+        res.json(results)
+      }
+    })
+  } catch (error) {
+    return res.status(500).json({
+      error,
+    })
+  }
+});
+
+// GET a single user by id
+router.get("/api/user/:id", async (req, res) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM user WHERE user_Id = ?";
+  try {
+    await connection.query(sql, [id], (error, results) => {
+      if (error) {
+        console.error(error.sqlMessage);
+      } else {
+        res.json(results);
+      }
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error,
+    });
+  }
+});
+
+// ADD a user
+router.post('/api/user', async(req, res) => {
+  const sql = 'INSERT INTO user (userName, userEmail, userPassword) VALUES (?,?,?)';
+  const params = [req.body.userName, req.body.userEmail, req.body.userPassword]
+
+  try{
+      await connection.query(sql, params, function (error, results, fields) {
+          if (error) {
+              if (error) throw error
+          }
+          return res.status(201).json({
+              success: true,
+              error: '',
+              message: 'Du har lagt till en ny användare!'
+          })
+      })
+  }catch(error){
+      return res.status(500).json({
+          success: false,
+          error: error.message,
+      })
+  }
+});
+
+// CHANGE user info
+ router.put('/api/user', async(req, res) => {  
+  const sql ='UPDATE user SET userName = ?, userEmail = ?, userPassword = ? WHERE user_Id = ?'
+  const params =[req.body.userName, req.body.userEmail, req.body.userPassword, req.body.user_Id]
+
+  try{
+      await connection.query(sql, params, function (error, results, fields) {
+          if (error) {
+              if (error) throw error
+          }
+          return res.status(201).json({
+              success: true,
+              error: '',
+              message: 'Du har ändrat användar-informationen'
+          })
+      })
+  }catch(error){
+      return res.status(500).json({
+          success: false,
+          error: error.message,
+      })
+  }
+});
+
+// DELETE user
+router.delete('/api/user', async(req, res) => {
+  console.log(req.body)
+  let sql = 'DELETE FROM user WHERE user_Id = ?'
+
+  try{
+      await connection.query(sql, [req.body.user_Id], function (error, results, fields) {
+          if (error) {
+              if (error) throw error
+          }
+          return res.status(201).json({
+              success: true,
+              error: '',
+              message: 'Användaren är nu raderad!'
+          })
+      })
+  }catch(error){
+      return res.status(500).json({
+          success: false,
+          error: error.message,
+      })
+  }
+});
+
+
+module.exports = router
