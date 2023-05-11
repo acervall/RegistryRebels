7378e3b1258738b45d538e465b71682740d5d01c
Marcus Oskarsson <MarcusOskarsson@hotmail.com>
2023-05-11 21:04:02 +0200

    adds + icon

--- frontend/src/components/AddProduct.vue
+++ frontend/src/components/AddProduct.vue
@@ -78,13 +78,23 @@
           }),
         }
         if (this.validated) {
-          const data = await fetch(
-            'http://localhost:3000/api/product/1',
-            options,
-          )
-          const res = await data.json()
-          if (res.success === true) {
-            this.addConfirmation = true
+          try {
+            const data = await fetch(
+              `http://localhost:3000/api/product/${this.listId}`,
+              options,
+            )
+            const res = await data.json()
+            if (res.success === true && data.ok) {
+              this.addConfirmation = true
+            } else {
+              console.log('STATUS: ', data.status, 'MSG: ', res.message)
+              throw new Error({ status: data.status, message: res.message })
+            }
+          } catch (error) {
+            console.table(error)
+            // if (error.status === 409) {
+            this.classRemoveFrom('name')
+            // }
           }
         } else {
           console.log('Needs more data')
@@ -202,19 +212,7 @@
       <span @click.prevent="addProduct" class="btn">
         Save
         <Transition name="first-render">
-          <svg
-            v-if="addConfirmation"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="green"
-            class="w-5 h-5"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          <v-icon v-if="addConfirmation" name="fc-checkmark" />
         </Transition>
       </span>
     </div>
@@ -372,7 +370,7 @@
     font-size: 0.75rem;
     height: 2.4rem;
     outline: none;
-    padding: 8px;
+    padding: 10px;
     width: 100%;
   }
 
@@ -390,7 +388,7 @@
     cursor: pointer;
     padding: 0.5rem;
     min-width: 25px;
-    svg {
+    icon {
       height: 1rem;
     }
   }
@@ -485,10 +483,11 @@
 
   // Transitions
   .input-accepted {
-    border-color: green;
+    // border-color: green;
   }
   .input-declined {
-    border-color: red;
+    border: 1px solid red;
+    color: red;
   }
 
   .list-enter-active,
