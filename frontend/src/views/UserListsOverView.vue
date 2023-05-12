1850c408eb0ffa2963c2cba661a3e55484d86b35
Thiti <thiti.m90@gmail.com>
2023-05-12 15:15:37 +0200

    navigation

--- frontend/src/views/UserListsOverView.vue
+++ frontend/src/views/UserListsOverView.vue
@@ -50,6 +50,19 @@
 
 <template>
   <main>
+    <router-link to="/userhome">
+      <svg
+        class="svg-left"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 25 25"
+      >
+        <path
+          style="fill: #767676"
+          d="M24 12.001H2.914l5.294-5.295-.707-.707L1 12.501l6.5 6.5.707-.707-5.293-5.293H24v-1z"
+          data-name="Left"
+        />
+      </svg>
+    </router-link>
     <h1>My lists</h1>
     <router-link to="/userlistadd">
       <input id="new-list-btn" type="button" value="Create new list" />
